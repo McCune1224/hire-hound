6a5d42848590c5658031c951927717c919878da6
McCune1224 <alexmccune1224@gmail.com>
2023-03-27 23:50:38 -0700

    Added Password validation and hashing

--- backend/handlers/auth.go
+++ backend/handlers/auth.go
@@ -1,6 +1,13 @@
 package handlers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"hirehound/models"
+	"hirehound/repository"
+	"regexp"
+
+	"github.com/gofiber/fiber/v2"
+	"golang.org/x/crypto/bcrypt"
+)
 
 func Login(c *fiber.Ctx) error {
 	return c.JSON(
@@ -19,9 +26,65 @@ func Logout(c *fiber.Ctx) error {
 }
 
 func Register(c *fiber.Ctx) error {
-	return c.JSON(
-		fiber.Map{
-			"message": "TODO: Logout",
-		},
-	)
+	requestUser := &models.User{}
+	userCreateErr := c.BodyParser(requestUser)
+	if userCreateErr != nil {
+		return c.Status(400).JSON(fiber.Map{
+			"message": "Invalid request body",
+			"error":   userCreateErr.Error(),
+		})
+	}
+
+	dbUser := &models.User{}
+	repository.DB.Where("email = ?", requestUser.Email).First(dbUser)
+	if dbUser.ID != 0 {
+		return c.Status(400).JSON(fiber.Map{
+			"message": "User already exists with that email",
+		})
+	}
+
+	// validate password length
+	if len(requestUser.Password) < 8 || len(requestUser.Password) > 20 {
+		return c.Status(400).JSON(fiber.Map{
+			"message": "Password must be between 8 and 20 characters",
+		})
+	}
+
+	// check if password has number
+	pattern := regexp.MustCompile(`[0-9]`)
+	if !pattern.MatchString(requestUser.Password) {
+		return c.Status(400).JSON(fiber.Map{
+			"message": "Password must contain at least one number",
+		})
+	}
+
+	// check if password has special character
+	pattern = regexp.MustCompile(`[!@#$%^&*]`)
+	if !pattern.MatchString(requestUser.Password) {
+		return c.Status(400).JSON(fiber.Map{
+			"message": "Password must contain at least one special character",
+		})
+	}
+
+	passHash, err := bcrypt.GenerateFromPassword([]byte(requestUser.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return c.Status(500).JSON(fiber.Map{
+			"message": "Failed to hash password",
+			"error":   err.Error(),
+		})
+	}
+	requestUser.Password = string(passHash)
+
+	userCreateErr = repository.DB.Create(requestUser).Error
+	if userCreateErr != nil {
+		return c.Status(500).JSON(fiber.Map{
+			"message": "Failed to create user",
+			"error":   userCreateErr.Error(),
+		})
+	}
+
+	return c.JSON(fiber.Map{
+		"message": "User created successfully",
+		"user":    requestUser,
+	})
 }
--- backend/handlers/auth_test.go
+++ backend/handlers/auth_test.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"bytes"
 	"encoding/json"
+	"hirehound/models"
 	"hirehound/repository"
 	"net/http/httptest"
 	"testing"
@@ -46,11 +47,43 @@ func TestRegister(t *testing.T) {
 				Email:    "testuser@gmail.com",
 			},
 		},
+		{
+			Description:        "Bad password (too short)",
+			ExpectedStatusCode: 400,
+			ExpectedResponse:   "Password must be between 8 and 20 characters",
+			RequestBody: reqBody{
+				Username: "testuser",
+				Password: "Test21!",
+				Email:    "foobarbaz@gmail.com",
+			},
+		},
+		{
+			Description:        "Bad password (no number)",
+			ExpectedStatusCode: 400,
+			ExpectedResponse:   "Password must contain at least one number",
+			RequestBody: reqBody{
+				Username: "testuser",
+				Password: "TestPassword!",
+				Email:    "foobarbaz@gmail.com",
+			},
+		},
+		{
+			Description:        "Bad password (no special character)",
+			ExpectedStatusCode: 400,
+			ExpectedResponse:   "Password must contain at least one special character",
+			RequestBody: reqBody{
+				Username: "testuser",
+				Password: "TestPassword21",
+				Email:    "foobarbaz@gmail.com",
+			},
+		},
 	}
 
 	test_app := appSetup()
 	test_app.Post("/register", Register)
 	for _, tc := range testCases {
+		// delete the user from the database just in case it exists already
+		repository.DB.Where("email = ?", tc.RequestBody.Email).Delete(&models.User{})
 
 		jsonOut, err := json.Marshal(tc.RequestBody)
 		if err != nil {
@@ -61,11 +94,14 @@ func TestRegister(t *testing.T) {
 			httpReq := httptest.NewRequest("POST", "/register", bytes.NewBuffer(jsonOut))
 			httpReq.Header.Set("Content-Type", "application/json")
 			httpResp, err := test_app.Test(httpReq)
+			jsonBodyResponse := make(map[string]interface{})
+			json.NewDecoder(httpResp.Body).Decode(&jsonBodyResponse)
+
 			if err != nil {
 				t.Errorf("Error testing app: %v", err)
 			}
 			if httpResp.StatusCode != tc.ExpectedStatusCode {
-				t.Errorf("Expected status code %d, got %d", tc.ExpectedStatusCode, httpResp.StatusCode)
+				t.Errorf("Expected status code %d, got %d\nMessage:%v", tc.ExpectedStatusCode, httpResp.StatusCode, jsonBodyResponse)
 			}
 		})
 	}
